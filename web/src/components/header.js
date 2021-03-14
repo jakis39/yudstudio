6a191ed06144e90023083b9ee0b6dfae4e806568
Jake Nusca <jakis39@gmail.com>
2021-03-13 20:00:43 -0500

    Improve mobile nav drawer

--- web/src/components/header.js
+++ web/src/components/header.js
@@ -1,40 +1,43 @@
-import {Link} from 'gatsby'
-import React from 'react'
-import Icon from './icon'
-import {cn} from '../lib/helpers'
+import { Link } from "gatsby";
+import React from "react";
+import Icon from "./icon";
+import { cn } from "../lib/helpers";
 
-import styles from './header.module.css'
+import styles from "./header.module.css";
 
-import logo from '../assets/yud.png';
-import PillButton from './pill-button'
+import logo from "../assets/yud.png";
+import PillButton from "./pill-button";
 
-const Header = ({onHideNav, onShowNav, showNav, siteTitle}) => (
+const Header = ({ onHideNav, onShowNav, showNav, siteTitle }) => (
   <div className={styles.root}>
-    <div className={styles.wrapper}>
+    <nav className={cn(styles.nav, showNav && styles.showNav)}>
+      <ul>
+        <li>
+          <PillButton to="/projects/" variant="outlined">
+            Work
+          </PillButton>
+        </li>
+        <li>
+          <PillButton to="/shop/" variant="outlined">
+            Shop
+          </PillButton>
+        </li>
+      </ul>
+    </nav>
 
+    <div className={styles.wrapper}>
       <button className={styles.toggleNavButton} onClick={showNav ? onHideNav : onShowNav}>
-        <Icon symbol='hamburger' />
+        <Icon symbol="hamburger" />
       </button>
 
-      <nav className={cn(styles.nav, showNav && styles.showNav)}>
-        <ul>
-          <li>
-            <PillButton to='/projects/' variant="outlined">Work</PillButton>
-          </li>
-          <li>
-            <PillButton to='/shop/' variant="outlined">Shop</PillButton>
-          </li>
-        </ul>
-      </nav>
-
       <div className={styles.branding}>
-        <Link to='/' className={styles.logo}>
+        <Link to="/" className={styles.logo}>
           <img src={logo} />
-          {/* <embed src={logo} width="200px" height="100px" type="image/svg+xml" />  */}
+          {/* <embed src={logo} width="200px" height="100px" type="image/svg+xml" /> */}
         </Link>
       </div>
     </div>
   </div>
-)
+);
 
-export default Header
+export default Header;
