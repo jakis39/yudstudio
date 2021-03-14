ef4b2db6f212c7ff3dbda29ed7957ff92a37d476
Jake Nusca <jakis39@gmail.com>
2021-03-13 20:48:05 -0500

    Rearrange classnames in project template to see if it fixes mobile class issues

--- web/src/components/project.js
+++ web/src/components/project.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactPlayer from "react-player";
 import Container from "./container";
+import classNames from 'classnames';
 
 import styles from "./project.module.css";
 import { responsiveTitle1, responsiveTitle2, paragraph } from "../components/typography.module.css";
@@ -21,13 +22,13 @@ function Project(props) {
             height="100%"
           />
         </div>
-        <h1 className={`${responsiveTitle1} ${styles.header}`}>{title}</h1>
-        <p className={`${responsiveTitle2} ${styles.excerpt}`}>{excerpt}</p>
+        <h1 className={classNames(responsiveTitle1, styles.header)}>{title}</h1>
+        <p className={classNames(responsiveTitle2, styles.excerpt)}>{excerpt}</p>
         <div className={styles.contributorsContainer}>
           {contributors &&
             contributors.length > 0 &&
             contributors.map((contributor) => (
-              <span key={contributor._key} className={`${styles.contributorBlock} ${paragraph}`}>
+              <span key={contributor._key} className={classNames(paragraph, styles.contributorBlock)}>
                 {`${contributor.role.title} - ${contributor.contributors}`}
               </span>
             ))}
