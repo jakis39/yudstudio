8e802bc8820c70fa1fbb7b1f556631150186b721
Jake Nusca <jakis39@gmail.com>
2021-03-22 23:36:07 -0400

    Stack project links at the top of hte page on mobile Force layout to 100vh on mobile so that content scrolls but header and footer are sticky

--- web/src/components/layout.js
+++ web/src/components/layout.js
@@ -1,19 +1,20 @@
-import React from 'react'
-import Header from './header'
+import React from 'react';
+import Header from './header';
 
-import '../styles/layout.css'
-import * as styles from './layout.module.css'
-import Container from './container'
+import '../styles/layout.css';
+import * as styles from './layout.module.css';
+import Container from './container';
 import classNames from 'classnames';
 
 const footerText = '© 2021 YUD STUDIO';
 
-const Layout = ({children, onHideNav, onShowNav, showNav, siteTitle, centered = false}) => (
+const Layout = ({ children, onHideNav, onShowNav, showNav, siteTitle, centered = false }) => (
   <div className={styles.pageWrapper}>
     <Header siteTitle={siteTitle} onHideNav={onHideNav} onShowNav={onShowNav} showNav={showNav} />
+
     <div className={styles.content}>{children}</div>
 
-    <footer className={classNames(styles.footer, {[styles.centeredFooter]: centered})}>
+    <footer className={classNames(styles.footer, { [styles.centeredFooter]: centered })}>
       {centered && footerText}
       {!centered && (
         <Container wide short>
@@ -22,6 +23,6 @@ const Layout = ({children, onHideNav, onShowNav, showNav, siteTitle, centered = 
       )}
     </footer>
   </div>
-)
+);
 
-export default Layout
+export default Layout;
